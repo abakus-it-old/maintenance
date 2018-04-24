1f591fe9667bc0a18ed0fa5ef87903fc1d39fcc9
Paul Ntabuye Butera <pntabuye@gmail.com>
2018-04-24 17:42:41 +0200

    [FIX] Dependancy and Label

--- maintenance_service_security_check/__openerp__.py
+++ maintenance_service_security_check/__openerp__.py
@@ -8,6 +8,7 @@
         'contract_timesheet_activities_on_site_management',
         'hr_analytic_timesheet_improvements',
         'account_analytic_account_improvements',
+        'portal_improvements',
     ],
     'author': "AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
